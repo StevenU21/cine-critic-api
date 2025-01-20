69a57aff3c858c6964b137b83665e6058e4e6510
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-20 15:40:55 -0600

    Add role creation and permission assignment to RolesAndPermissionsSeeder

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
@@ -12,23 +13,33 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create permissions
         $permissions = [
-            'create genre',
-            'read genre',
-            'update genre',
-            'delete genre',
-            'create user',
-            'read user',
-            'update user',
-            'delete user',
-            'assign role',
-            'unassign role',
-            'assign permission',
-            'unassign permission',
+            'create genre', 'read genre', 'update genre', 'delete genre',
+            'create user', 'read user', 'update user', 'delete user',
+            'assign role', 'unassign role', 'assign permission', 'unassign permission',
         ];
 
+        // Save permissions to database
         foreach ($permissions as $permission) {
             Permission::firstOrCreate(['name' => $permission]);
         }
+
+        // Create roles
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $moderatorRole = Role::firstOrCreate(['name' => 'moderator']);
+        $reviewerRole = Role::firstOrCreate(['name' => 'reviewer']);
+
+        //Assign permissions to roles
+        $adminRole->givePermissionTo($permissions);
+
+        $moderatorRole->givePermissionTo([
+            'create genre', 'read genre', 'update genre', 'delete genre',
+            'create user', 'read user', 'update user', 'delete user',
+        ]);
+
+        $reviewerRole->givePermissionTo([
+            'read genre', 'read user',
+        ]);
     }
 }
