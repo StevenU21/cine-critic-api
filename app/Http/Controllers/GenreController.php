d447a995ad99506123a8e48849d94a87ed8c068b
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-19 23:42:50 -0600

    Implement index method in GenreController to paginate and return genres as a resource collection

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Genre;
+use App\Http\Resources\GenreResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Http\Requests\GenreRequest;
 
 class GenreController extends Controller
 {
-    //
+    public function index(): AnonymousResourceCollection
+    {
+        $genres = Genre::latest()->paginate(10);
+        return GenreResource::collection($genres);
+    }
 }
