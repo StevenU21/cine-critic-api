018e9c89e03ed4f7b1e77d52b4d6ef2fbfcce805
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-20 14:51:46 -0600

    Add feature tests for GenreController to verify user access to genre list and specific genre details

--- tests/Feature/GenreControllerTest.php
+++ tests/Feature/GenreControllerTest.php
@@ -2,18 +2,37 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Genre;
 
 class GenreControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+
+    public function test_user_can_view_genre_list()
     {
-        $response = $this->get('/');
+        Genre::factory(10)->create();
+
+        $user = User::factory()->create();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $response = $this->withHeader('Authorization', "Bearer $token")->get('/api/genres');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_show_specify_genre()
+    {
+        $genre = Genre::factory()->create();
+
+        $user = User::factory()->create();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $response = $this->withHeader('Authorization', "Bearer $token")->get("/api/genres/$genre->id");
 
         $response->assertStatus(200);
     }
