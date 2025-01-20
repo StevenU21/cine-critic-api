afef536a4be90b81f75d687092fa2ad4fbc12b5a
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-20 00:42:31 -0600

    Implement authorization for viewing genres and set up policy in AppServiceProvider; add GenreControllerTest for basic functionality

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\Genre;
 use App\Http\Resources\GenreResource;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use App\Http\Requests\GenreRequest;
 
 class GenreController extends Controller
 {
+    use AuthorizesRequests;
+
     public function index(): AnonymousResourceCollection
     {
+        $this->authorize('viewAny', Genre::class);
+        
         $genres = Genre::latest()->paginate(10);
         return GenreResource::collection($genres);
     }
