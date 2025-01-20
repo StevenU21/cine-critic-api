c6cb8df36581bb5f18bc8a88072fb15b99c7eec8
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-19 23:31:41 -0600

    Update GenreRequest to authorize requests and add validation rules with custom messages

--- app/Http/Requests/GenreRequest.php
+++ app/Http/Requests/GenreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class GenreRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class GenreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,26 @@ class GenreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'min:6', 'max:30', Rule::unique('genres')->ignore($this->genre)],
+            'description' => ['nullable', 'string', 'max:255'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'name.string' => 'The name field must be a string.',
+            'name.min' => 'The name field must be at least :min characters.',
+            'name.max' => 'The name field may not be greater than :max characters.',
+            'name.unique' => 'The name field already exists.',
+            'description.string' => 'The description field must be a string.',
+            'description.max' => 'The description field may not be greater than :max characters.',
         ];
     }
 }
