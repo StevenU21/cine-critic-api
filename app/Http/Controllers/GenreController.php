52b85aa8d265e372663d0e3321f4bfcb69b4121f
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-19 23:23:29 -0600

    Add Genre model, controller, policy, factory, and migration

---
+++ app/Http/Controllers/GenreController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Genre;
+use App\Http\Requests\StoreGenreRequest;
+use App\Http\Requests\UpdateGenreRequest;
+
+class GenreController extends Controller
+{
+    //
+}
