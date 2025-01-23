7ccaa40ae2cc91d3b33b23b7c462f0c73ceb5fc2
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-22 19:12:58 -0600

    Add example test for admin user permission list view

--- tests/Feature/Admin/PermissionTest.php
+++ tests/Feature/Admin/PermissionTest.php
@@ -6,5 +6,9 @@ use Tests\TestCase;
 
 class PermissionTest extends TestCase
 {
-    //
+    //example test
+    public function test_admin_user_can_view_permission_list()
+    {
+        $this->assertTrue(false);
+    }
 }
