da16375db2d94d518246cfa7029f26daa51bee71
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-22 19:00:58 -0600

    Remove unused test files for cleanup

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace Tests\Feature\Admin;
+namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class UserTest extends TestCase
+class ExampleTest extends TestCase
 {
-
     //
 }
