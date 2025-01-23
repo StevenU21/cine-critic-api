4a3132173c78828418c7ce9443f558d88bd89c1f
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-23 14:57:06 -0600

    Remove unnecessary whitespace in show method of UserController for cleaner code

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
     public function show(User $user): UserResource
     {
         $this->authorize('view', $user);
-        
+
         $user->load(['roles.permissions']);
 
         return new UserResource($user);
