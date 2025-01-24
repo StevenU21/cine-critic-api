73cab3b16513dace70f897e46cdd52fbc9ff11f8
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-24 12:25:09 -0600

    Add migrations for movies and directors tables with relationships

--- database/migrations/2025_01_24_120340_create_movies_table.php
+++ database/migrations/2025_01_24_120340_create_movies_table.php
@@ -4,15 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('movies', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('cover_image');
+            $table->string('release_date');
+
+            $table->integer('director_id')->unsigned();
+            $table->foreign('director_id')->references('id')->on('directors')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('genre_id')->unsigned();
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
