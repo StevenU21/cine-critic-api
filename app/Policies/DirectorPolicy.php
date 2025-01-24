e854285f6ae1e7029710dbf4346fad2fabf1b1b3
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-24 14:40:42 -0600

    Implement authorization checks in DirectorPolicy and remove unused RatingFactory

--- app/Policies/DirectorPolicy.php
+++ app/Policies/DirectorPolicy.php
@@ -2,15 +2,58 @@
 
 namespace App\Policies;
 
+use App\Exceptions\AuthorizationException;
 use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
 class DirectorPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
+    use HandlesAuthorization;
+
+    public function before(User $user, $ability)
     {
-        //
+        if ($user->hasRole('admin')) {
+            return true;
+        }
+    }
+
+    public function viewAny(User $user): bool
+    {
+        if (!$user->hasPermissionTo('read directors')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function view(User $user): bool
+    {
+        if (!$user->hasPermissionTo('read directors')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        if (!$user->hasPermissionTo('create directors')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function update(User $user): bool
+    {
+        if (!$user->hasPermissionTo('update directors')) {
+            throw new AuthorizationException();
+        }
+        return true;
+    }
+
+    public function delete(User $user): bool
+    {
+        if (!$user->hasPermissionTo('delete directors')) {
+            throw new AuthorizationException();
+        }
+        return true;
     }
 }
