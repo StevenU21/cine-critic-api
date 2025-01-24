5c71da7fccd7cbaca6f2ec49f340cc4466cd3fa4
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-24 14:38:14 -0600

    Update DirectorRequest validation rules and enable authorization

--- app/Http/Requests/DirectorRequest.php
+++ app/Http/Requests/DirectorRequest.php
@@ -11,7 +11,7 @@ class DirectorRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,36 @@ class DirectorRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'min:3', 'max:30'],
+            'biography' => ['required', 'string', 'min:3', 'max:2000'],
+            'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg, webp', 'max:4096'],
+            'birth_date' => ['required', 'date', 'before:today', 'after:01-01-1890', 'date_format:d-m-Y'],
+            'nationality' => ['required', 'string', 'max:30'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'name.string' => 'The name field must be a string.',
+            'name.min' => 'The name field must be at least :min characters.',
+            'name.max' => 'The name field may not be greater than :max characters.',
+            'biography.required' => 'The biography field is required.',
+            'biography.string' => 'The biography field must be a string.',
+            'biography.min' => 'The biography field must be at least :min characters.',
+            'biography.max' => 'The biography field may not be greater than :max characters.',
+            'image.required' => 'The image field is required.',
+            'image.string' => 'The image field must be a string.',
+            'birth_date.required' => 'The birth date field is required.',
+            'birth_date.date' => 'The birth date field must be a date.',
+            'birth_date.before' => 'The birth date field must be a date before today.',
+            'birth_date.after' => 'The birth date field must be a date after 01-01-1890.',
         ];
     }
 }
