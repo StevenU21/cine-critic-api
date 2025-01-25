f7f61a0fc7b463cd8d126fc79327aff6df5548af
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-25 01:51:14 -0600

    Refactor MovieResource to conditionally include ID and timestamps for admin users; remove price field from Movie model

--- app/Http/Resources/MovieResource.php
+++ app/Http/Resources/MovieResource.php
@@ -14,6 +14,18 @@ class MovieResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $user = $request->user();
+        return [
+            'id' => $this->when($user && $user->hasRole('admin') && $request->has('include_id'), $this->id),
+            'title' => $this->title,
+            'description' => $this->description,
+            'image_url' => $this->image_url,
+            'release_date' => $this->release_date,
+            'genre_id' => GenreResource::make($this->whenLoaded('genre')),
+            'director_id' => DirectorResource::make($this->whenLoaded('director')),
+            'average_rating' => $this->averageRating(),
+            'created_at' => $this->when($user && $user->hasRole('admin') && $request->has('include_timestamps'), $this->created_at->format('Y-m-d H:i:s')),
+            'updated_at' => $this->when($user && $user->hasRole('admin') && $request->has('include_timestamps'), $this->updated_at->format('Y-m-d H:i:s')),
+        ];
     }
 }
