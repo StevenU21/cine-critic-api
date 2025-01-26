df77f4b1243ea41fe3d7fd4aca84c2790a48b293
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-25 22:37:48 -0600

    Refactor ReviewController to use ReviewRequest for validation and enhance authorization for store, update, and destroy methods

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\ReviewResource;
 use App\Models\Review;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -35,24 +37,43 @@ class ReviewController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ReviewRequest $request): ReviewResource
     {
-        //
+        $this->authorize('create', Review::class);
+
+        $review = Review::create($request->validated() + [
+            'movie_id' => $request->movie_id,
+            'user_id' => $request->user_id,
+        ]);
+
+        return new ReviewResource($review);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ReviewRequest $request, int $id): ReviewResource
     {
-        //
+        $review = Review::findOrFailCustom($id);
+        $this->authorize('update', $review);
+
+        $review->update($request->validated() + [
+            'movie_id' => $request->movie_id,
+            'user_id' => $request->user_id,
+        ]);
+
+        return new ReviewResource($review);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(int $id): JsonResponse
     {
-        //
+        $review = Review::findOrFailCustom($id);
+        $this->authorize('delete', $review);
+        $review->delete();
+
+        return response()->json(null, 204);
     }
 }
