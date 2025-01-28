24fcfe852982198a1ed3195c934a57dc63f22cea
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-27 20:37:39 -0600

    Add activity logging to Genre model for tracking changes to name and description

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -6,16 +6,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\FindModelOrFail;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Genre extends Model
 {
     /** @use HasFactory<\Database\Factories\GenreFactory> */
-    use HasFactory, FindModelOrFail;
+    use HasFactory, FindModelOrFail, LogsActivity;
 
     protected $fillable = [
         'name',
         'description'
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly(['name', 'description']);
+    }
 
     public function movies(): HasMany
     {
