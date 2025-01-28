15f9240b2037fabf224093174fb10cd1165d2f9d
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-01-27 20:37:07 -0600

    Add activity logging to Director model for tracking changes to specific attributes

--- app/Models/Director.php
+++ app/Models/Director.php
@@ -6,10 +6,12 @@ use App\Traits\FindModelOrFail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Director extends Model
 {
-    use HasFactory, FindModelOrFail;
+    use HasFactory, FindModelOrFail, LogsActivity;
 
     protected $fillable = [
         'name',
@@ -18,6 +20,12 @@ class Director extends Model
         'birth_date',
         'nationality',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'biography', 'image', 'birth_date', 'nationality']);
+    }
 
     public function movies(): HasMany
     {
